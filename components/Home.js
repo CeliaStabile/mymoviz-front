1df5760b67ac61927a81532337dc209c042839c3
Celia Stabile <celia-stabile@hotmail.fr>
2023-03-19 15:28:36 +0100

    clean up and comments

--- components/Home.js
+++ components/Home.js
@@ -18,6 +18,7 @@ function Home() {
     }
   };
 
+  //peupler les liked movies dans le contenu du popover
   const likedMoviesPopover = likedMovies.map((data, i) => {
     return (
       <div key={i} className={styles.likedMoviesContainer}>
@@ -33,15 +34,7 @@ function Home() {
     </div>
   );
 
-  // Movies list A REMPLACER PAR BOUCLE ET FETCH API
-  // const moviesData = [
-  //   { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
-  //   { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
-  //   { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
-  //   { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
-  //   { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
-  // ];
-
+//mettre en place les cartes movies via l'api themoviedatabase
   const [moviesData, setMoviesData] = useState([]);
 
  useEffect(() => {
